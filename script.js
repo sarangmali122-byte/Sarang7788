8ad4a833bf3865856f7de5510ae30de6885e29aa
sarangmali122-byte <sarangmali122@gmail.com>
2026-04-10 01:28:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,20 @@ closeBtn.onclick = function(){
 lightbox.style.display="none";
 
 };
+
+const sections=document.querySelectorAll("section");
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(sec=>{
+
+const top=window.scrollY;
+const offset=sec.offsetTop-300;
+
+if(top>offset){
+sec.classList.add("show");
+}
+
+});
+
+});
